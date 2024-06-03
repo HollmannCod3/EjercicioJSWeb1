0e4e5526009c78f2c0989be0280e67492a41d64c
Nicolas Hollmann <nicolashollmanncod3@gmail.com>
2024-06-03 12:06:12 -0300

    Terminado el formulario, con styles y las verificaciones en js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('formulario');
     form.addEventListener('submit', (event) => {
@@ -24,31 +27,33 @@ function validar() {
     const errores = [];
 
     if (nombre.length < 3) {
-        errores.push('El nombre debe tener al menos 3 caracteres');
+        errores.push({ id: 'nombre', mensaje: 'El nombre debe tener al menos 3 caracteres'});
     }
 
     const telefonoRegex = /^[0-9]{10}$/;
     if (!telefonoRegex.test(telefono)) {
-        errores.push('El teléfono debe tener 10 dígitos numéricos');
+        errores.push({id: 'telefono', mensaje: 'El teléfono debe tener 10 dígitos numéricos'});
+    }
+
+    document.querySelectorAll('.error-message').forEach(el => el.textContent = '');
+    document.querySelectorAll('.inputes').forEach(el => el.classList.remove('error'));
+
+
+    if (errores.length > 0) {
+        errores.forEach(error => {
+            const errorElement = document.getElementById(`${error.id}-error`);
+            const inputElement = document.getElementById(error.id);
+            errorElement.textContent = error.mensaje;
+            inputElement.classList.add('error');
+        });
+        document.getElementById('resultado').style.display = 'none';
+
+        return;
     }
 
     const resultadoDiv = document.getElementById('resultado');
     resultadoDiv.innerHTML = ''; // Limpiar el contenido anterior
     resultadoDiv.style.display = 'block';
-
-    if (errores.length > 0) {
-        const errorTitle = document.createElement('h3');
-        errorTitle.textContent = 'Errores: ';
-        resultadoDiv.appendChild(errorTitle);
-
-        errores.forEach(error => {
-            const errorP = document.createElement('p');
-            errorP.className = 'error';
-            errorP.textContent = error;
-            resultadoDiv.appendChild(errorP);
-        });
-        return;
-    }
 
     // Si todas las validaciones son correctas, mostrar los datos
     const successMessage = `
