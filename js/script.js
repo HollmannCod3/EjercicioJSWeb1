d238db3e0ca624fee0c2ab2d2271cfe9490f1e99
Nicolas Hollmann <nicolashollmanncod3@gmail.com>
2024-06-03 19:08:34 -0300

    Parcial de web

--- js/script.js
+++ js/script.js
@@ -4,35 +4,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('formulario');
     form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Evita el envío del formulario
+        event.preventDefault(); 
         validar();
     });
 
-    const tempRange = document.getElementById('temperatura');
-    tempRange.addEventListener('input', mostrarTemp);
-    mostrarTemp(); // Inicializar el valor en el span al cargar la página
+    const puntRango = document.getElementById('reseña');
+    puntRango.addEventListener('input', mostrarReseña);
+    mostrarReseña();
 });
 
-function mostrarTemp() {
-    const temp = document.getElementById('temperatura').value;
-    document.getElementById('tempOutput').textContent = temp;
+function mostrarReseña() {
+    const puntuacion = document.getElementById('reseña').value;
+    document.getElementById('pun').textContent = puntuacion;
 }
 
 function validar() {
     const nombre = document.getElementById('nombre').value;
-    const telefono = document.getElementById('telefono').value;
-    const temperatura = document.getElementById('temperatura').value;
+    const comentario = document.getElementById('comentario').value;
+    const reseña = document.getElementById('reseña').value;
 
-    // Validaciones básicas
+    
     const errores = [];
 
     if (nombre.length < 3) {
         errores.push({ id: 'nombre', mensaje: 'El nombre debe tener al menos 3 caracteres'});
     }
 
-    const telefonoRegex = /^[0-9]{10}$/;
-    if (!telefonoRegex.test(telefono)) {
-        errores.push({id: 'telefono', mensaje: 'El teléfono debe tener 10 dígitos numéricos'});
+    if (reseña < 1 || reseña > 5) {
+        errores.push({ id: 'reseña', mensaje: 'La puntuación debe estar entre 1 y 5.' });
+    }
+
+    if(comentario.length > 0){
+    if (comentario.length > 200) {
+        errores.push({id: 'comentario', mensaje: 'El comentario debe contener menos de 200 caracteres.'});
+    }else if (comentario.length < 10){
+        errores.push({id: 'comentario', mensaje: 'El comentario tiene que contener más de 10 caracteres.'});
+    }
     }
 
     document.querySelectorAll('.error-message').forEach(el => el.textContent = '');
@@ -52,15 +59,16 @@ function validar() {
     }
 
     const resultadoDiv = document.getElementById('resultado');
-    resultadoDiv.innerHTML = ''; // Limpiar el contenido anterior
+    resultadoDiv.innerHTML = ''; 
     resultadoDiv.style.display = 'block';
 
-    // Si todas las validaciones son correctas, mostrar los datos
+
     const successMessage = `
-        <h4>Datos Enviados: </h4>
-        <p class="success"><strong>Nombre:</strong> ${nombre}</p>
-        <p class="success"><strong>Teléfono:</strong> ${telefono}</p>
-        <p class="success"><strong>Temperatura:</strong> ${temperatura}°C</p>
+        <h4>INFORMACIÓN SOBRE RESEÑA: </h4>
+        <p class="success"><strong>NOMBRE DE PELÍCULA:</strong> ${nombre}</p>
+        <p class="success"><strong>RESEÑA:</strong> ${reseña} </p>
+        <p class="success"><strong>COMENTARIO:</strong> ${comentario}</p>
+        
     `;
     resultadoDiv.innerHTML = successMessage;
     resultadoDiv.scrollIntoView({ behavior: 'smooth' });
